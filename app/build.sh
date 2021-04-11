39086b231b036965f5186ef42d2712347d26f4d1
restato <direcision@gmail.com>
2021-04-11 22:38:36 +0900

    change proejct structure

--- app/build.sh
+++
@@ -1,2 +0,0 @@
-docker run -it ubuntu:14.04 /bin/bash
-docker run -it -v $(pwd):/app realestate:latest /bin/bash
--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
 docker run -it ubuntu:14.04 /bin/bash
-
 docker run -it -v $(pwd):/app realestate:latest /bin/bash
-
-
-pip install streamlit
